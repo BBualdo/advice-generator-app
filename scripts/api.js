89a894e1cc55cd9e407acb832d673747c9a7ee04
Sebastian <mrseb10@gmail.com>
2023-08-12 15:33:22 +0200

    Get data from API and some CSS improvements

--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,17 @@
+const adviceNumber = document.querySelector('h1');
+const adviceContent = document.querySelector('h2');
+const diceButton = document.querySelector('button');
+
 const API_ADVICE_URL = "https://api.adviceslip.com/advice";
+
+diceButton.addEventListener('click', () => {
+  fetch(API_ADVICE_URL)
+  .then(res => res.json())
+  .then(data => {
+    const id = data.slip.id;
+    const advice = data.slip.advice;
+    adviceNumber.innerHTML = `Advice #${id}`;
+    adviceContent.innerHTML = `"${advice}"`;
+  })
+})
+
